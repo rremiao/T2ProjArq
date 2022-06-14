1902521051c8912be99352ab325672622a1a8467
Wagner <Wagnerburgie@gmail.com>
2022-06-13 21:28:51 -0300

    Adicionado docker file

--- produtoservice/src/test/java/com/rremiao/produtoservice/ProdutoserviceApplicationTests.java
+++ produtoservice/src/test/java/com/rremiao/produtoservice/ProdutoserviceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.rremiao.produtoservice;
+// package com.rremiao.produtoservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ProdutoserviceApplicationTests {
+// @SpringBootTest
+// class ProdutoserviceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
